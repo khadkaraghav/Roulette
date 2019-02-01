65dd54da17b275741490a4ee9e29bb76d639ab70
khadkaraghav <khadkaraghav@gmail.com>
2019-02-01 13:23:51 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,157 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        rouletteGame();
+
+    }
+
+    public static void rouletteGame() {
+
+        int total, wonGame, lostGame;
+        double betNum = 0;
+        int numOne = 0;
+        double lostBet = 0;
+
+
+        ArrayList<String> colorPick = new ArrayList<>();
+
+        colorPick.add("black");
+        colorPick.add("red");
+
+        Scanner input = new Scanner(System.in);
+
+        Random rnd = new Random();
+
+        System.out.println("Welcome! Let's play some roulette");
+
+        while (true) {
+
+            System.out.println("Enter the type of bet you would like to place (red|black|even|odd|high|low):");
+
+            String choice = input.nextLine();
+
+            if (choice.equalsIgnoreCase("red")) {
+
+                System.out.println("Enter in your bet amount:");
+
+                betNum = input.nextDouble();
+
+                numOne = 1 + rnd.nextInt(36);
+
+                Collections.shuffle(colorPick);
+
+                if (colorPick.get(0) == "black") {
+                    System.out.println("The ball landed on" + " " + numOne + " " + colorPick.get(0) + "\n" + "Sorry, you've lost this bet.");
+                    lostBet = 0 - betNum;
+                    System.out.println("You currently have $" + " " + lostBet);
+                } else {
+                    System.out.println("The ball landed on" + " " + numOne + " " + colorPick.get(0) + "\n" + "Congratulations, you've won.");
+                    System.out.println("You currently have $" + " " + betNum);
+                }
+            } else if (choice.equalsIgnoreCase("black")) {
+
+                System.out.println("Enter in your bet amount:");
+
+                betNum = input.nextDouble();
+
+                numOne = 1 + rnd.nextInt(36);
+
+                Collections.shuffle(colorPick);
+
+                if (colorPick.get(0) == "red") {
+                    System.out.println("The ball landed on" + " " + numOne + " " + colorPick.get(0) + "\n" + "Sorry, you've lost this bet.");
+                    lostBet = 0 - betNum;
+                    System.out.println("You currently have $" + " " + lostBet);
+                } else {
+                    System.out.println("The ball landed on" + " " + numOne + " " + colorPick.get(0) + "\n" + "Congratulations, you've won.");
+                    System.out.println("You currently have $" + " " + betNum);
+                }
+            } else if (choice.equalsIgnoreCase("even")) {
+
+                System.out.println("Enter in your bet amount:");
+
+                betNum = input.nextDouble();
+
+                numOne = 1 + rnd.nextInt(36);
+
+                Collections.shuffle(colorPick);
+
+                if (numOne % 2 == 0) {
+                    System.out.println("The ball landed on" + " " + numOne + " " + colorPick.get(0) + "\n" + "Congratulations, you've won.");
+                    System.out.println("You currently have $" + " " + betNum);
+                } else {
+                    System.out.println("The ball landed on" + " " + numOne + " " + colorPick.get(0) + "\n" + "Sorry, you've lost this bet.");
+                    lostBet = 0 - betNum;
+                    System.out.println("You currently have $" + " " + lostBet);
+                }
+            } else if (choice.equalsIgnoreCase("odd")) {
+
+                System.out.println("Enter in your bet amount:");
+
+                betNum = input.nextDouble();
+
+                numOne = 1 + rnd.nextInt(36);
+
+                Collections.shuffle(colorPick);
+
+                if (numOne % 2 != 0) {
+                    System.out.println("The ball landed on" + " " + numOne + " " + colorPick.get(0) + "\n" + "Congratulations, you've won.");
+                    System.out.println("You currently have $" + " " + betNum);
+                } else {
+                    System.out.println("The ball landed on" + " " + numOne + " " + colorPick.get(0) + "\n" + "Sorry, you've lost this bet.");
+                    lostBet = 0 - betNum;
+                    System.out.println("You currently have $" + " " + lostBet);
+                }
+            } else if (choice.equalsIgnoreCase("low")) {
+
+                System.out.println("Enter in your bet amount:");
+
+                betNum = input.nextDouble();
+
+                numOne = 1 + rnd.nextInt(36);
+
+                Collections.shuffle(colorPick);
+
+                if ((numOne > 1) & (numOne < 18)) {
+                    System.out.println("The ball landed on" + " " + numOne + " " + colorPick.get(0) + "\n" + "Congratulations, you've won.");
+                    System.out.println("You currently have $" + " " + betNum);
+                } else {
+                    System.out.println("The ball landed on" + " " + numOne + " " + colorPick.get(0) + "\n" + "Sorry, you've lost this bet.");
+                    lostBet = 0 - betNum;
+                    System.out.println("You currently have $" + " " + lostBet);
+                }
+
+            } else if (choice.equalsIgnoreCase("high")) {
+
+                System.out.println("Enter in your bet amount:");
+
+                betNum = input.nextDouble();
+
+                numOne = 1 + rnd.nextInt(36);
+
+                Collections.shuffle(colorPick);
+
+                if ((numOne > 19) & (numOne < 36)) {
+                    System.out.println("The ball landed on" + " " + numOne + " " + colorPick.get(0) + "\n" + "Congratulations, you've won.");
+                    System.out.println("You currently have $" + " " + betNum);
+                } else {
+                    System.out.println("The ball landed on" + " " + numOne + " " + colorPick.get(0) + "\n" + "Sorry, you've lost this bet.");
+                    lostBet = 0 - betNum;
+                    System.out.println("You currently have $" + " " + lostBet);
+                }
+            }
+        }
+    }
+}
+
+
+
+
